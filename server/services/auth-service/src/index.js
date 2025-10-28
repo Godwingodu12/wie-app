21745abd6a900f7d78497a74097bc4bafa082c8f
Gokul Gopalan <106225510+apsgokulag@users.noreply.github.com>
2025-10-28 19:15:05 +0530

    Update index.js

--- server/services/auth-service/src/index.js
+++ server/services/auth-service/src/index.js
@@ -6,26 +6,35 @@ import authRoutes from './routes/auth.routes.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
+import session from 'express-session';
+import passport  from './config/passport.config.js';
 import { connectRabbitMQ } from './rabbit/connection.js';
 import { startConsumers } from './rabbit/index.js';
-
-// 👇 Needed for __dirname in ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-// Config
 dotenv.config();
 const app = express();
-// Middleware
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+const uploadsPath = path.join(__dirname, 'uploads');
+app.use('/uploads', express.static(uploadsPath));
 
 // Routes
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'your-secret-key-change-in-production',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === 'production', // true in production with HTTPS
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const startServer = async () => {
   try {
