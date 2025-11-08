53aa22a2599fe8f485c92476b2b037cadc163ac0
Gokul Gopalan <106225510+apsgokulag@users.noreply.github.com>
2025-11-09 03:45:15 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store,persistor } from './features/store';
+import { store, persistor } from './features/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import './index.css'
-import App from './App.jsx'
-
+import { SocketProvider } from './context/SocketContext';
+import './index.css';
+import App from './App.jsx';
 createRoot(document.getElementById('root')).render(
- <Provider store={store}>
-  <PersistGate loading={null} persistor={persistor}>
-    <App />
-  </PersistGate>
-  </Provider>
-)
-
-
-
-
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <SocketProvider>
+          <App />
+        </SocketProvider>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
