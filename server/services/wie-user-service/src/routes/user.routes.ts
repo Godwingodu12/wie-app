dfa553f267e6409ab4cafc564c2dc523fc15ee1b
Gokul Gopalan <106225510+apsgokulag@users.noreply.github.com>
2025-11-14 21:31:55 +0530

    Update user.routes.ts

--- server/services/wie-user-service/src/routes/user.routes.ts
+++ server/services/wie-user-service/src/routes/user.routes.ts
@@ -1,24 +1,15 @@
 import express from 'express';
-import {
-  index,
-  getCountries,
-  signupSendOtp,
-  signupVerifyOtp,
-  login,
-  resendOtp,
-  updateProfile,
-} from '../services/wie-user.service';
+import { index,getCountries,signupSendOtp,signupVerifyOtp,login,resendOtp,getProfile,updateProfile,googleAuth,googleCallback  } from '../services/wie-user.service';
 import { authenticateToken } from '../middlewares/auth.middleware';
 const router: express.Router = express.Router();
-// Public routes
 router.get('/', index);
 router.get('/countries', getCountries);
 router.post('/signup/send-otp', signupSendOtp);
 router.post('/signup/verify-otp', signupVerifyOtp);
 router.post('/login', login);
 router.post('/resend-otp', resendOtp);
-
-// Protected routes
-router.put('/profile/:userId', authenticateToken, updateProfile);
-
+router.get('/google/auth', googleAuth);
+router.get('/google/callback', googleCallback);
+router.get('/get-profile', authenticateToken, getProfile);
+router.put('/update-profile', authenticateToken, updateProfile);
 export default router;
