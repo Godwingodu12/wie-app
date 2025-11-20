ae5718da267debb3a1c147affed4189f0a66c3e4
Gokul Gopalan <106225510+apsgokulag@users.noreply.github.com>
2025-11-21 02:39:46 +0530

    Update ticket.routes.ts

--- server/services/wie-user-service/src/routes/ticket.routes.ts
+++ server/services/wie-user-service/src/routes/ticket.routes.ts
@@ -1,10 +1,5 @@
 import express from 'express';
-import {
-  getLiveEvents,
-  getActiveGroups,
-  getTicket,
-  getGroup,getNearbyEvents,
-} from '../controllers/ticket.controller';
+import { getLiveEvents,getActiveGroups,getTicket,getGroup,getNearbyEvents,getCategoryBasedEvents } from '../controllers/ticket.controller';
 const router: express.Router = express.Router();
 // PUBLIC routes - No authentication required
 router.get('/live-events', getLiveEvents);
@@ -12,4 +7,5 @@ router.get('/get-active-groups', getActiveGroups);
 router.get('/event/:ticketId', getTicket);
 router.get('/group/:groupId', getGroup);
 router.get('/nearby-events', getNearbyEvents);
+router.get('/category-events', getCategoryBasedEvents);
 export default router;
