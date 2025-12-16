9886d88b707b7f35bafe0af24cbadb237029bd1b
Gokul Gopalan <106225510+apsgokulag@users.noreply.github.com>
2025-12-16 17:39:19 +0530

    Update layout.tsx

--- client-user/src/app/layout.tsx
+++ client-user/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Provider } from 'react-redux';
 import { store } from '@/features/store';
+import NotificationInitializer from '@/components/notifications/NotificationInitializer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Provider store={store}>
-          {children}
+          <NotificationInitializer>{children}</NotificationInitializer>
         </Provider>
       </body>
     </html>
