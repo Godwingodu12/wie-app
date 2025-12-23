2669d9019519c6edba97d75b02fb254a3fa0c9a1
Gokul Gopalan <106225510+apsgokulag@users.noreply.github.com>
2025-12-23 21:05:34 +0530

    Update layout.tsx

--- client-user/src/app/(protected)/layout.tsx
+++ client-user/src/app/(protected)/layout.tsx
@@ -1,12 +1,17 @@
 'use client';
+
+import { SidebarProvider } from '@/context/SidebarContext';
+
 export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      <main className="flex-grow">{children}</main>
-    </div>
+    <SidebarProvider>
+      <div className="flex flex-col min-h-screen">
+        <main className="flex-grow">{children}</main>
+      </div>
+    </SidebarProvider>
   );
 }
