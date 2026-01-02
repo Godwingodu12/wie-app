fad6fb10002bedeb72f86ccf8043d9e563dfeaba
Gokul Gopalan <106225510+apsgokulag@users.noreply.github.com>
2026-01-02 22:13:48 +0530

    Update layout.tsx

--- client-user/src/app/(protected)/layout.tsx
+++ client-user/src/app/(protected)/layout.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { restoreAuth } from '@/features/auth/authSlice';
 import { SidebarProvider } from '@/context/SidebarContext';
 
 export default function ProtectedLayout({
@@ -7,6 +10,12 @@ export default function ProtectedLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(restoreAuth());
+  }, [dispatch]);
+
   return (
     <SidebarProvider>
       <div className="flex flex-col min-h-screen">
