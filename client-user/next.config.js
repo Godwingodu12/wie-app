02d40303210472909c45733c2b177aaf0e9d2811
Gokul Gopalan <106225510+apsgokulag@users.noreply.github.com>
2026-01-02 21:53:12 +0530

    Update next.config.js

--- client-user/next.config.js
+++ client-user/next.config.js
@@ -1,8 +1,38 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'lh3.googleusercontent.com', 'res.cloudinary.com'],
+    remotePatterns: [
+      // Cloudinary
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      // Google profile pictures
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      // Google storage
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/**',
+      },
+      // AWS S3 (if you use it later)
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+        pathname: '/**',
+      },
+      // Local development
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        pathname: '/**',
+      },
+    ],
   },
-  reactStrictMode: true,
-  // Remove the experimental.appDir - it's no longer needed in Next.js 14
-};
-module.exports = nextConfig;
+}
+module.exports = nextConfig
